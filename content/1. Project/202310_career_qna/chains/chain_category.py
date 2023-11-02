0570d430896f13aa9262a903b43bc53de580a73d
rho715 <rho715@wavve.com>
2023-11-02 17:19:20 +0900

    Quartz sync: Nov 2, 2023, 5:19 PM

--- content/1. Project/202310_career_qna/chains/chain_category.py
+++
@@ -1,5 +0,0 @@
-from packages.prompts import CATEGORY_PROMPT
-from packages.models import CHAT_LLM
-from langchain.schema.output_parser import StrOutputParser
-
-category_chain = CATEGORY_PROMPT | CHAT_LLM | StrOutputParser()
